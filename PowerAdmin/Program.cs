3e043be63825f6558024917a6c8da717507a156d
drsh <41671372+drawmoon@users.noreply.github.com>
2021-12-06 16:45:04 +0800

    first commit

---
+++ PowerAdmin/Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// 配置 Furion
+builder.Inject();
+
+// Add services to the container.
+
+var services = builder.Services;
+
+services.AddControllers();
+
+// 添加 Furion 服务
+services.AddInject();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+// 使用 Furion 中间件
+app.UseInject("api");
+
+app.MapControllers();
+
+app.Run();
