cbd9d309ed13831b2ef2bd7fb58918a2ad43f10c
drawmoon <1340260725@qq.com>
2021-12-06 16:45:04 +0800

    first commit

---
+++ PowerAdmin/Controllers/ProfileController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PowerAdmin.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProfile()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
