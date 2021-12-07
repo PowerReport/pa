37f1704ba26e6656b0ff7cdadd8727cc4b74d7f4
drawmoon <1340260725@qq.com>
2021-12-07 18:23:49 +0800

    提交连接 Postgres 数据库模块

--- src/PowerAdmin.EntityFramework.Identity/Repositories/IdentityRepository.cs
+++ src/PowerAdmin.EntityFramework.Identity/Repositories/IdentityRepository.cs
@@ -1,7 +1,9 @@
-﻿using PowerAdmin.EntityFramework.Identity.Repositories.Interfaces;
+﻿using PowerAdmin.EntityFramework.Identity.Entities;
+using PowerAdmin.EntityFramework.Identity.Repositories.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,5 +11,9 @@ namespace PowerAdmin.EntityFramework.Identity.Repositories
 {
     public class IdentityRepository : IIdentityRepository
     {
+        public Task<UserIdentity> GetProfile(ClaimsPrincipal user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- src/PowerAdmin.EntityFramework.Identity/Repositories/Interfaces/IIdentityRepository.cs
+++ src/PowerAdmin.EntityFramework.Identity/Repositories/Interfaces/IIdentityRepository.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using PowerAdmin.EntityFramework.Identity.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,6 @@ namespace PowerAdmin.EntityFramework.Identity.Repositories.Interfaces
 {
     public interface IIdentityRepository
     {
+        Task<UserIdentity> GetProfile(ClaimsPrincipal user);
     }
 }
