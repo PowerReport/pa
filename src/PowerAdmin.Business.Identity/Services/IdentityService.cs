d6b4e0d2a1d0fe62b68c5270caccad658a806989
drawmoon <1340260725@qq.com>
2021-12-07 13:59:08 +0800

    提交业务层，类库的引用关系

---
+++ src/PowerAdmin.Business.Identity/Services/IdentityService.cs
@@ -0,0 +1,13 @@
+﻿using PowerAdmin.Business.Identity.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerAdmin.Business.Identity.Services
+{
+    public class IdentityService : IIdentityService
+    {
+    }
+}
