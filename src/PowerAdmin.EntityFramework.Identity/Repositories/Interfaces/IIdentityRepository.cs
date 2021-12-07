8386b6e981e141a5d0f6a2c9fc2b7ad11e621fff
drsh <41671372+drawmoon@users.noreply.github.com>
2021-12-07 13:52:17 +0800

    提交用户身份实体

---
+++ src/PowerAdmin.EntityFramework.Identity/Repositories/Interfaces/IIdentityRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerAdmin.EntityFramework.Identity.Repositories.Interfaces
+{
+    public interface IIdentityRepository
+    {
+    }
+}
