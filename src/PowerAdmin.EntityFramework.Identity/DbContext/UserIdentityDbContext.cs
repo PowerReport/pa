a0f7ee4e0b4150cbb2f465904e60a107a3ec7ac2
drsh <41671372+drawmoon@users.noreply.github.com>
2021-12-07 14:18:03 +0800

    提交数据库上下文

---
+++ src/PowerAdmin.EntityFramework.Identity/DbContext/UserIdentityDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PowerAdmin.EntityFramework.Identity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerAdmin.EntityFramework.Identity.DbContext
+{
+    public class UserIdentityDbContext : IdentityDbContext<UserIdentity, UserIdentityRole, string, UserIdentityClaim, UserIdentityUserRole, UserIdentityUserLogin, UserIdentityRoleClaim, UserIdentityUserToken>
+    {
+        public UserIdentityDbContext(DbContextOptions<UserIdentityDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<UserIdentity>().ToTable("users");
+            builder.Entity<UserIdentityClaim>().ToTable("claims");
+            builder.Entity<UserIdentityUserRole>().ToTable("user_roles");
+
+            builder.Entity<UserIdentityRole>().ToTable("roles");
+            builder.Entity<UserIdentityRoleClaim>().ToTable("role_claims");
+
+            builder.Entity<UserIdentityUserLogin>().ToTable("user_logins");
+            builder.Entity<UserIdentityUserToken>().ToTable("user_tokens");
+        }
+    }
+}
