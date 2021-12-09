0b823283d17922abb783c262e0484f9f49737895
drsh <41671372+drawmoon@users.noreply.github.com>
2021-12-09 18:18:13 +0800

    提交用户信息的数据传输类

---
+++ src/PowerAdmin.Business.Identity/Dtos/Identity/Base/BaseUserDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerAdmin.Business.Identity.Dtos.Identity.Base
+{
+    public class BaseUserDto<TUserId>
+    {
+        /// <summary>
+        /// 用户的唯一标识
+        /// </summary>
+        public TUserId Id { get; set; }
+    }
+}
