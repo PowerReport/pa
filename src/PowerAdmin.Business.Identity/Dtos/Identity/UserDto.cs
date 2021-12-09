73e66851cb20ca57248e361d9e743524882f6502
drawmoon <1340260725@qq.com>
2021-12-09 18:18:13 +0800

    提交用户信息的数据传输类

---
+++ src/PowerAdmin.Business.Identity/Dtos/Identity/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using PowerAdmin.Business.Identity.Dtos.Identity.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerAdmin.Business.Identity.Dtos.Identity
+{
+    public class UserDto : UserDto<string>
+    {
+        public UserDto(string userName) : base(userName)
+        {
+        }
+    }
+}
