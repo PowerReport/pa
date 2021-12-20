485e89778d20e1af8343dd28f75589fb80396241
drsh <41671372+drawmoon@users.noreply.github.com>
2021-12-20 14:21:37 +0800

    修复应用配置不生效的问题

--- src/PowerAdmin.UI/Program.cs
+++ src/PowerAdmin.UI/Program.cs
@@ -1,3 +1,4 @@
+using AntDesign.ProLayout;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PowerAdmin.UI;
 using PowerAdmin.UI.Models;
@@ -7,6 +8,7 @@ builder.RootComponents.Add<App>("#app");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddAntDesign();
-builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("ProSettings"));
+builder.Services.Configure<ProSettings>(builder.Configuration.GetSection("ProSettings"));
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("App"));
 
 await builder.Build().RunAsync();
