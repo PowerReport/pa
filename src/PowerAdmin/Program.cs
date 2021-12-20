674df86e45aa6078c4978cd5ac670197f0ceed54
drawmoon <1340260725@qq.com>
2021-12-20 12:26:23 +0800

    将日志组件更替为 Serilog

--- src/PowerAdmin/Program.cs
+++ src/PowerAdmin/Program.cs
@@ -1,30 +1,47 @@
 using PowerAdmin.Business.Identity.Extensions;
 using PowerAdmin.EntityFramework.Configuration.Extensions;
 using PowerAdmin.EntityFramework.Shared.DbContexts;
+using Serilog;
+using Serilog.Events;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // 配置 Furion
 builder.Inject();
+// 配置 Serilog
+builder.WebHost.UseSerilogDefault(options =>
+{
+#if DEBUG
+    options.MinimumLevel.Debug()
+#else
+    options.MinimumLevel.Information()
+#endif
+        .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+        .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+        .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+        .WriteTo.Console()
+        .WriteTo.File("logs/application.log", LogEventLevel.Information, rollingInterval: RollingInterval.Day, retainedFileCountLimit: null, encoding: Encoding.UTF8);
+});
 
 // Add services to the container.
 
-// 添加数据库上下文
+// 注册数据库上下文
 builder.Services.AddDbContexts<UserIdentityDbContext>();
 
-// 添加用户身份服务
+// 注册用户身份服务
 builder.Services.AddIdentityServices<UserIdentityDbContext>();
 
 builder.Services.AddControllers();
 
-// 添加 Furion 服务
+// 注册 Furion 服务
 builder.Services.AddInject();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-// 数据库迁移
+// 使用数据库迁移
 app.EnsureDatabasesMigrated<UserIdentityDbContext>();
 
 // 使用静态文件，默认路径是 wwwroot
