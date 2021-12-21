fc48e59d7561c880427170f57767989f7160a2cf
drsh <41671372+drawmoon@users.noreply.github.com>
2021-12-21 14:36:26 +0800

    提交创建初始管理员账号

---
+++ src/PowerAdmin.EntityFramework.Configuration/Configuration/Identity/Claim.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerAdmin.EntityFramework.Configuration.Configuration.Identity
+{
+    public class Claim
+    {
+        public string Type { get; set; } = string.Empty;
+
+        public string Value { get; set; } = string.Empty;
+    }
+}
---
+++ src/PowerAdmin.EntityFramework.Configuration/Configuration/Identity/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerAdmin.EntityFramework.Configuration.Configuration.Identity
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+
+        public string? Password { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string? Email { get; set; }
+
+        public List<string> Roles { get; set; } = new List<string>();
+
+        public List<Claim> Claims { get; set; } = new List<Claim>();
+    }
+}
